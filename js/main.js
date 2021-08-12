1d2e811b19c7ec65f7d1c9d470a32605080f432b
Leo Maia <oleomaia@gmail.com>
2021-08-12 19:14:01 +0200

    Added modal enlarging images in the illustrations section.

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//
+// Load images in a modal
+//
+
+// Store body to apply styles when show the modal
+const body = document.querySelector(`body`);
+
+// Store all images we plan to show in a Modal
+const modalImages = document.querySelectorAll(`.gallery li img`);
+
+// Store modal element
+const modal = document.querySelector(`.modal`);
+
+// Select all elements inside the modal markup template
+const modalElement = element => document.querySelector(`.modal ${element}`);
+
+// For each item we want to be able to open in a modal
+modalImages.forEach(img => {
+	const data = img.dataset
+	img.addEventListener(`click`, e => {
+		body.style.overflow = `hidden`
+		e.stopPropagation()
+		modal.style.display = `flex`
+		modalElement(`img`).src = img.src
+	})
+});
+
+// Click anywhere to close the modal
+document.addEventListener(`click`, () => {
+	body.style.overflow = `auto`
+	modal.style.display = `none`
+}) 
